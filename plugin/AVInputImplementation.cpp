a94240a4732cbdfd6f9c77ed96683c6470e403bb
sreeja <sreeja_nampoothiri@comcast.com>
2026-02-13 16:34:19 +0530

    CMFSUPPORT-3807: COVERITY TEST. DO NOT MERGE

--- plugin/AVInputImplementation.cpp
+++ plugin/AVInputImplementation.cpp
@@ -1242,7 +1242,7 @@ namespace Plugin {
                 memcpy(&pre, spdVect.data(), sizeof(struct dsSpd_infoframe_st));
 
                 char str[200] = { 0 };
-                snprintf(str, sizeof(str), "Packet Type:%02X,Version:%u,Length:%u,vendor name:%s,product des:%s,source info:%02X",
+                snprintf(str, sizeof(str), "Packet Type:%02X,Version:%u,Length:%u,vendor name:%s,product des:%s,source info:%02X,%d",
                     pre.pkttype, pre.version, pre.length, pre.vendor_name, pre.product_des, pre.source_info);
                 HDMISPD = str;
             }
