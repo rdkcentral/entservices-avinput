a22b25b534ec41e603d92aa95fa152b6c62b23b6
rajaLucy <rajakiranyadavalli@gmail.com>
2026-06-04 13:07:16 +0530

    Update testframework to 1.0.12 and fix entservices-helpers build

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -142,7 +142,8 @@ echo "building entservices-helpers"
 cmake -G Ninja -S ../entservices-helpers -B build/entservices-helpers \
     -DEXCEPTIONS_ENABLE=ON \
     -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
-    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake"
+    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    "-DCMAKE_CXX_FLAGS=-I$GITHUB_WORKSPACE/entservices-testframework/Tests/mocks -I$GITHUB_WORKSPACE/entservices-testframework/Tests/headers -I$GITHUB_WORKSPACE/entservices-testframework/Tests/headers/rdk/iarmbus -include $GITHUB_WORKSPACE/entservices-testframework/Tests/mocks/Iarm.h "
 cmake --build build/entservices-helpers --target install
 
 
